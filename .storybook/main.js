2f1abec75b6ddedd489c847444b2604603601b9f
Claudio Barsante <clbmribas@gmail.com>
2021-10-31 16:25:03 -0300

    fix: config webpack in main.js in folder .storybook to work with absolute path /src

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,9 @@
 module.exports = {
-  "stories": [
-    "../src/components/**/stories.mdx",
-    "../src/components/**/stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ['../src/components/**/stories.tsx'],
+  addons: ['@storybook/addon-essentials'],
 
-    "@storybook/addon-essentials"
-  ]
+  webpackFinal: (config) => {//to consider /src as root
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
