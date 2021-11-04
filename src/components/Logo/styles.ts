18b59245c020788ffd6c8f93a695b2a07b16460d
Claudio Barsante <clbmribas@gmail.com>
2021-11-04 17:41:17 -0300

    feat: finish <Logo /> with styles and tests

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,6 +1,16 @@
 import styled, { css } from 'styled-components';
 import { LogoProps } from '.';
+import media from 'styled-media-query';
 
+/** Default sizes styled-media-query
+ * {
+  huge: '1440px',
+  large: '1170px',
+  medium: '768px',
+  small: '450px',
+}
+
+ */
 const containerModifiers = {
   normal: () => css`
     width: 11rem;
@@ -10,14 +20,32 @@ const containerModifiers = {
   large: () => css`
     width: 20rem;
     height: 5.9rem;
+  `,
+
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+  width:5.8rem;
+  height:4.5rem;
+
+  svg {
+    height:4.5rem;
+    pointer-events:none;//Set whether or not an element should react to pointer events
+    //even if the image logo isn't cropped what is behind the image could be clicked
+  }
+
+  .text {//hide text from svg logo
+    display:none;
+  }
+  `}
   `
 };
 
 export const Container = styled.div<LogoProps>`
-  ${({ theme, color, size }) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
     color: ${theme.colors[
       color!
     ]}; // color! '!' not null assertion, you garantee that always will be a value
     ${!!size && containerModifiers[size]}
+    ${!!hideOnMobile && containerModifiers.hideOnMobile}
   `}
 `;
