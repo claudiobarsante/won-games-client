99d840bd227f1647c26a8d5ce3b47b4704a86a65
Claudio Barsante <clbmribas@gmail.com>
2021-11-10 19:47:39 -0300

    feat: add <Button/> element and it's sizes

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+import Button from '.';
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  }
+} as Meta;
+
+export const Default: Story = (args) => <Button {...args} />;
+
+// -- defining default values
+Default.args = {
+  children: 'Buy now'
+};
