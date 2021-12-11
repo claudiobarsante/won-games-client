fac20ee332e08d7ebae9172f1de0d951a154d88c
Claudio Barsante <clbmribas@gmail.com>
2021-12-11 13:34:49 -0300

    feat: add <Banner/> with tests

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,35 @@
+import { screen } from '@testing-library/react';
+
+import Banner from '.';
+import { renderWithTheme } from '../../utils/tests/helpers';
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+};
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    renderWithTheme(<Banner {...props} />);
+
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /Play the new CrashLands season/ })
+    ).toBeInTheDocument();
+
+    expect(screen.getByRole('img')).toHaveAttribute('aria-label', 'Defy death');
+    expect(
+      screen.getByRole('img', { name: /Defy death/i })
+    ).toBeInTheDocument();
+
+    expect(screen.getByRole('link', { name: /Buy now/i })).toHaveAttribute(
+      'href',
+      '/games/defy-death'
+    );
+  });
+});
