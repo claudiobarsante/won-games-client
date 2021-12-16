f52868df1b6f593c338193c45956c6cdaf3c78d5
Claudio Barsante <clbmribas@gmail.com>
2021-12-16 15:40:59 -0300

    feat: add <Banner/> and <Ribbon/>

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,11 +1,16 @@
 import * as S from './styles';
 import Button from './../Button';
+import Ribbon, { RibbonSizes, RibbonColors } from 'components/Ribbon';
+
 export type BannerProps = {
   img: string;
   title: string;
   subtitle: string;
   buttonLabel: string;
   buttonLink: string;
+  ribbon?: React.ReactNode;
+  ribbonSize?: RibbonSizes;
+  ribbonColor?: RibbonColors;
 };
 
 const Banner = ({
@@ -13,19 +18,30 @@ const Banner = ({
   title,
   subtitle,
   buttonLabel,
-  buttonLink
+  buttonLink,
+  ribbon,
+  ribbonSize = 'normal',
+  ribbonColor = 'primary'
 }: BannerProps) => (
-  <S.Container>
-    <S.Image src={img} role="img" aria-label={title} />
+  <>
+    <S.Container>
+      {!!ribbon && (
+        <Ribbon color={ribbonColor} size={ribbonSize}>
+          {ribbon}
+        </Ribbon>
+      )}
 
-    <S.Caption>
-      <S.Title>{title}</S.Title>
-      <S.Subtitle dangerouslySetInnerHTML={{ __html: subtitle }} />
-      <Button as="a" href={buttonLink} size="large">
-        {buttonLabel}
-      </Button>
-    </S.Caption>
-  </S.Container>
+      <S.Image src={img} role="img" aria-label={title} />
+
+      <S.Caption>
+        <S.Title>{title}</S.Title>
+        <S.Subtitle dangerouslySetInnerHTML={{ __html: subtitle }} />
+        <Button as="a" href={buttonLink} size="large">
+          {buttonLabel}
+        </Button>
+      </S.Caption>
+    </S.Container>
+  </>
 );
 
 export default Banner;
