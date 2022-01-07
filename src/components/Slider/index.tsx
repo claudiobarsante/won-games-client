8a0462237307715cfac54078a801550d86a0fd8e
Claudio Barsante <clbmribas@gmail.com>
2022-01-07 16:29:53 -0300

    feat: <Slider/> component

---
+++ src/components/Slider/index.tsx
@@ -0,0 +1,17 @@
+import SlickSlider, { Settings } from 'react-slick';
+import * as S from './styles';
+
+export type SliderSettings = Settings;
+
+export type SliderProps = {
+  children: React.ReactNode;
+  settings: SliderSettings;
+};
+
+const Slider = ({ children, settings }: SliderProps) => (
+  <S.Container>
+    <SlickSlider {...settings}>{children}</SlickSlider>
+  </S.Container>
+);
+
+export default Slider;
---
+++ src/components/Slider/test.tsx
@@ -0,0 +1,29 @@
+// -- Important this file has to be the first one imported
+import '../../../.jest/match-media-mock';
+// -- has to mock the matchmedia because the tests are running in the jsdom and it don't have all the features
+// --
+import { render, screen } from '@testing-library/react';
+
+import Slider from '.';
+
+describe('<Slider />', () => {
+  it('should render children as slider item', () => {
+    const { container } = render(
+      <Slider settings={{ slidesToShow: 1, infinite: false }}>
+        <p>Item 1</p>
+        <p>Item 2</p>
+      </Slider>
+    );
+
+    // -- parentElement?.parentElement has to go 2 levels up
+    expect(
+      screen.getByText(/item 1/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide');
+
+    expect(
+      screen.getByText(/item 2/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide');
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
