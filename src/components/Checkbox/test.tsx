580f6a46caeff3ef37b5a9b9b8519f0b7482b4e2
Claudio Barsante <clbmribas@gmail.com>
2022-01-26 12:43:14 -0300

    feat: add styles to checkbox

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react';
+import { themeApp } from 'styles/theme';
 
 import Checkbox from '.';
 import { renderWithTheme } from './../../utils/tests/helpers';
@@ -18,5 +19,14 @@ describe('<Checkbox />', () => {
     renderWithTheme(<Checkbox />);
 
     expect(screen.queryByLabelText('Checkbox')).not.toBeInTheDocument();
+  });
+
+  it('should render with black label', () => {
+    renderWithTheme(
+      <Checkbox label="checkbox label" labelFor="check" labelColor="black" />
+    );
+    expect(screen.getByText(/checkbox label/i)).toHaveStyle({
+      color: themeApp.colors.black
+    });
   });
 });
