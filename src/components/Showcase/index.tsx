c0cb0674e1294142ffa8dea7818186a29f816858
Claudio Barsante <clbmribas@gmail.com>
2022-01-31 13:08:50 -0300

    refactor: Home page to use <Showcase/>

--- src/components/Showcase/index.tsx
+++ src/components/Showcase/index.tsx
@@ -1,9 +1,26 @@
+import GameCardSlider from 'components/GameCardSlider';
+import Heading from 'components/Heading';
+import { GameCardProps } from 'components/GameCard';
+import Highlight, { HighlightProps } from 'components/Highlight';
+
 import * as S from './styles';
 
-const Showcase = () => (
-  <S.Container>
-    <h1>Showcase</h1>
-  </S.Container>
-)
+export type ShowcaseProps = {
+  title?: string;
+  highlight?: HighlightProps;
+  games?: GameCardProps[];
+};
+
+const Showcase = ({ title, highlight, games }: ShowcaseProps) => (
+  <S.Wrapper>
+    {!!title && (
+      <Heading lineLeft lineColor="secondary">
+        {title}
+      </Heading>
+    )}
+    {!!highlight && <Highlight {...highlight} />}
+    {!!games && <GameCardSlider items={games} />}
+  </S.Wrapper>
+);
 
 export default Showcase;
--- src/components/Showcase/styles.ts
+++ src/components/Showcase/styles.ts
@@ -1,3 +1,29 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import media from 'styled-media-query';
 
-export const Container = styled.main``;
+import { Container } from 'components/Container';
+import * as HeadingStyles from 'components/Heading/styles';
+import * as GameCardSliderStyles from 'components/GameCardSlider/styles';
+import * as HighlightStyles from 'components/Highlight/styles';
+
+export const Wrapper = styled(Container).attrs({ as: 'section' })`
+  ${({ theme }) => css`
+    ${HeadingStyles.Container},
+    ${HighlightStyles.Container},
+    ${GameCardSliderStyles.Container} {
+      margin-bottom: ${theme.spacings.medium};
+    }
+    ${HighlightStyles.Container} {
+      ${media.lessThan('medium')`
+        margin-right: calc(-${theme.grid.gutter} / 2);
+        margin-left: calc(-${theme.grid.gutter} / 2);
+      `}
+    }
+    ${GameCardSliderStyles.Container} {
+      ${media.lessThan('huge')`
+        margin-right: calc(-${theme.grid.gutter} / 2);
+      `}
+    }
+    margin-bottom: calc(${theme.spacings.large} * 2);
+  `}
+`;
