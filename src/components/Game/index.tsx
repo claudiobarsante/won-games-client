c19da1fd1d2879929a026cb6b9ff09e47d8bbd6e
Claudio Barsante <clbmribas@gmail.com>
2022-02-04 16:53:27 -0300

    feat: add Game page template

--- src/components/Game/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from './styles';
-
-const Game = () => (
-  <S.Container>
-    <h1>Game</h1>
-  </S.Container>
-)
-
-export default Game;
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -8,6 +8,7 @@ export const Container = styled.menu`
 
     padding: ${theme.spacings.small} 0;
     position: relative;
+    z-index: ${theme.layers.menu};
   `}
 `;
 
