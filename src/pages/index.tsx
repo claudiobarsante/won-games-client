fd8b8b3fb31844c2695c77f5d8aa0d88e687030e
Claudio Barsante <clbmribas@gmail.com>
2022-03-10 15:44:05 -0300

    feat: add QueryHome

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,36 @@ import Home, { HomeTemplateProps } from 'templates/Home';
 import bannersMock from 'components/BannerSlider/mock';
 import gamesMock from 'components/GameCardSlider/mock';
 import highlightMock from 'components/Highlight/mock';
+import { initializeApollo } from 'utils/apollo';
+import { QueryHome } from 'graphql/generated/QueryHome';
+import { QUERY_HOME } from 'graphql/queries/home';
+import { GetStaticProps } from 'next';
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />;
 }
 
-export const getServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const apolloClient = initializeApollo();
+  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME });
+
+  const banners = data.banners.map((banner) => ({
+    img: `http://localhost:1337${banner.image?.url}`,
+    title: banner.title,
+    subtitle: banner.subtitle,
+    buttonLabel: banner.button?.label,
+    buttonLink: banner.button?.link,
+    ...(banner.ribbon && {
+      ribbon: banner.ribbon.text,
+      ribbonColor: banner.ribbon.color,
+      ribbonSize: banner.ribbon.size
+    })
+  }));
+
   return {
     props: {
-      banners: bannersMock,
+      revalidade: 60,
+      banners,
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
