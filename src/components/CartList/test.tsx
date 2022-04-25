4aef8dbc6db9140eca34681f4464f0d3eefda4e6
Claudio Barsante <clbmribas@gmail.com>
2022-04-25 19:47:53 -0300

    refactor: <CartList/>

--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -1,6 +1,7 @@
 // import { screen } from '@testing-library/react';
 // import { renderWithTheme } from 'utils/tests/helpers';
 
+import { CartContextDefaultValues } from 'hooks/use-cart';
 import { render, screen } from 'utils/test-utils';
 
 import CartList from '.';
@@ -8,9 +9,12 @@ import mockItems from './mock';
 
 describe('<CartList />', () => {
   it('should render the cart list', () => {
-    const { container } = render(
-      <CartList items={mockItems} total="R$ 330,00" />
-    );
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      items: mockItems,
+      total: 'R$ 330,00'
+    };
+    const { container } = render(<CartList />, { cartProviderProps });
 
     expect(screen.getAllByRole('heading')).toHaveLength(2);
     expect(screen.getByText('R$ 330,00')).toHaveStyle({ color: '#F231A5' });
@@ -19,7 +23,12 @@ describe('<CartList />', () => {
   });
 
   it('should render the button', () => {
-    render(<CartList items={mockItems} total="R$ 330,00" hasButton />);
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      items: mockItems
+    };
+
+    render(<CartList hasButton />, { cartProviderProps });
 
     expect(screen.getByText(/buy it now/i)).toBeInTheDocument();
   });
