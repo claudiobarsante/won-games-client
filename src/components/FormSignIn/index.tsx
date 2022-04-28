1cdd0460ccf688c04ad6e6e2f075026aaeae0547
Claudio Barsante <clbmribas@gmail.com>
2022-04-28 15:40:28 -0300

    feat: add <FormLoading/>

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -3,35 +3,65 @@ import { Email, Lock } from '@styled-icons/material-outlined';
 import TextField from 'components/TextField';
 import Button from 'components/Button';
 import Link from 'next/link';
-import { FormContainer, FormLink } from 'components/Form';
+import { FormContainer, FormLink, FormLoading } from 'components/Form';
+import { useState } from 'react';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
-const FormSignIn = () => (
-  <FormContainer>
-    <form>
-      <TextField
-        name="email"
-        placeholder="Email"
-        type="email"
-        icon={<Email />}
-      />
-      <TextField
-        name="password"
-        placeholder="Password"
-        type="password"
-        icon={<Lock />}
-      />
-      <S.ForgotPassword href="#">Forgot your password?</S.ForgotPassword>
-      <Button size="large" fullWidth>
-        Sign in now
-      </Button>
-      <FormLink>
-        Don’t have an account?{''}
-        <Link href="/sign-up">
-          <a>Sign up</a>
-        </Link>
-      </FormLink>
-    </form>
-  </FormContainer>
-);
+const FormSignIn = () => {
+  const [values, setValues] = useState({});
+  const [loading, setLoading] = useState(false);
+  const { push } = useRouter();
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    setLoading(true);
+    const result = await signIn('credentials', {
+      ...values,
+      redirect: false,
+      callbackUrl: '/'
+    });
+
+    if (result?.url) {
+      return push(result?.url);
+    }
+    setLoading(false);
+    console.error('email verification failed');
+  };
+
+  const handleInput = (field: string, value: string) => {
+    setValues((previous) => ({ ...previous, [field]: value }));
+  };
+  return (
+    <FormContainer>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="email"
+          placeholder="Email"
+          type="email"
+          onInputChange={(newValue) => handleInput('email', newValue)}
+          icon={<Email />}
+        />
+        <TextField
+          name="password"
+          placeholder="Password"
+          type="password"
+          onInputChange={(newValue) => handleInput('password', newValue)}
+          icon={<Lock />}
+        />
+        <S.ForgotPassword href="#">Forgot your password?</S.ForgotPassword>
+        <Button type="submit" size="large" fullWidth disabled={loading}>
+          {loading ? <FormLoading /> : <span>Sign in now</span>}
+        </Button>
+        <FormLink>
+          Don’t have an account?{''}
+          <Link href="/sign-up">
+            <a>Sign up</a>
+          </Link>
+        </FormLink>
+      </form>
+    </FormContainer>
+  );
+};
 
 export default FormSignIn;
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -21,44 +21,81 @@ const options: NextAuthOptions = {
     CredentialsProvider({
       name: 'Sign-in',
 
-      credentials: {},
-      async authorize({ email, password }: AuthorizeProps) {
+      credentials: {
+        email: {},
+        password: {}
+      },
+      async authorize(credentials, req) {
         const res = await fetch(
           `${process.env.NEXT_PUBLIC_API_URL}/auth/local`,
           {
             method: 'POST',
-            body: new URLSearchParams({ identifier: email, password })
+            body: new URLSearchParams({
+              identifier: credentials?.email as string,
+              password: credentials?.password as string
+            })
             // headers: { 'Content-Type': 'application/json' }
           }
         );
         const data = await res.json();
+        //console.log('data', data);
 
         // If no error and we have user data, return it
-        if (data.user) {
-          return { ...data.user, jwt: data.jwt };
-        }
+        // if (data.user) {
+        //   console.log('passei aqui');
+        //   return { ...data.user, jwt: data.jwt };
+        // }
+        if (data.user) return data;
         // Return null if user data could not be retrieved
         return null;
       }
     })
   ],
   callbacks: {
-    session: async (session: Session, user: User) => {
-      session.jwt = user.jwt;
-      session.id = user.id;
+    // session: async (session: Session, user: User) => {
+    //   console.log('userjwtinsession', user);
+    //   session.jwt = user.jwt;
+    //   session.id = user.id;
 
-      return Promise.resolve(session);
+    //   return Promise.resolve(session);
+    // },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      //session.jwt = user.jwt;
+      //session.id = user.id;
+
+      //console.log('session', session, 'token', token, 'user', user);
+      return session;
     },
-    jwt: async (token: JWT, user: User) => {
-      if (user) {
-        token.id = user.id;
-        token.email = user.email;
-        token.name = user.username as string;
-        token.jwt = user.jwt;
-      }
+    async jwt({ token, user, account, profile, isNewUser }) {
+      // console.log('--token', token);
 
-      return Promise.resolve(token);
+      // if (user) {
+      //   //console.log('--user dentro do if', user.id, user?.email);
+      //   const userData = { ...user };
+      //   console.log('userData', userData);
+
+      //   // token.id = userData.
+      //   token.email = userData.email;
+      //   // token.name = userData.username as string;
+      //   // token.jwt = userData.jwt;
+
+      //   console.log('new token', token);
+      // }
+
+      return token;
     }
+    // jwt: async (token: JWT, user: User) => {
+    //   console.log('userjwt', user);
+    //   if (user) {
+    //     token.id = user.id;
+    //     token.email = user.email;
+    //     token.name = user.username as string;
+    //     token.jwt = user.jwt;
+    //   }
+
+    //   return Promise.resolve(token);
+    // }
   }
 };
 
