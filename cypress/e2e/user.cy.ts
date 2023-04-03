e8b42fc77feb159268c78b60ce26473cf38fe334
Claudio Barsante <clbmribas@gmail.com>
2023-04-03 18:16:52 -0300

    tests: test user and redirect to the page that was previously defined

--- cypress/e2e/user.cy.ts
+++ cypress/e2e/user.cy.ts
@@ -1,8 +1,8 @@
-import { cy, Cypress, expect, describe, it } from 'local-cypress';
+import { cy, Cypress, describe, it } from 'local-cypress';
 import { createUser } from '../support/generate';
 
 describe('User', () => {
-  it.skip('should sign up', () => {
+  it('should sign up', () => {
     const user = createUser();
 
     cy.visit('/sign-up');
@@ -34,5 +34,24 @@ describe('User', () => {
 
     cy.findByRole('link', { name: /sign in/i }).should('exist');
     cy.findByText(/cypress/i).should('not.exist');
+  });
+
+  it.only('should sign the user and redirect to the page that was previously defined', () => {
+    cy.visit('/profile/me');
+    // -- redirect to sing-in with the 'profile/me' callback
+    cy.location('href').should(
+      'eq',
+      Cypress.config().baseUrl + '/sign-in?callbackUrl=/profile/me'
+    );
+
+    cy.findByPlaceholderText(/email/i).type('e2e@wongames.com');
+    cy.findByPlaceholderText(/password/i).type('123456');
+    cy.findByRole('button', { name: /sign in now/i }).click();
+    // -- redirect to profile/me
+    cy.location('href').should('eq', Cypress.config().baseUrl + '/profile/me');
+    // -- confirm user
+    cy.findByLabelText(/username/i).should('have.value', 'cypress');
+    // -- or cy.get('#username').should('have.value', 'cypress');
+    cy.findByLabelText(/e-mail/i).should('have.value', 'e2e@wongames.com');
   });
 });
